d2e455ec8f6ff96483224235e9d7b3cb400404aa
Rudra Shankar <rudra.s@trans.com>
2024-01-16 12:20:16 +0530

    Updated2

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from typing import List
 
 #Declaring variables for setup function
 PROJECT_NAME="housing-predictor"
-VERSION="0.0.1"
+VERSION="0.0.5"
 AUTHOR="Rudra Shankar"
 DESCRIPTION="This is my first end-to-end ML project"
-PACKAGES=["housing"]
-REQUIREMENT_FILE_NAME="requirements.txt"
 
+REQUIREMENT_FILE_NAME="requirements.txt"
+SKLEARN_ALLOW_DEPRECATED_SKLEARN_PACKAGE_INSTALL=True
 
 def get_requirements_list()->List[str]:
     """
@@ -19,7 +19,7 @@ def get_requirements_list()->List[str]:
     in requirements.txt file
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 
 
 setup(
@@ -27,8 +27,7 @@ name=PROJECT_NAME,
 version=VERSION,
 author=AUTHOR,
 description=DESCRIPTION,
-packages=PACKAGES,
+packages=find_packages(),
 install_requires=get_requirements_list()
-
 )
 
